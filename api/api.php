ecfc73d947a4f13dc684726debea2bef2c4f059d
Nele M <nelemeyer43@gmail.com>
2025-05-22 10:37:34 +0200

    error handling api.php

--- api/api.php
+++ api/api.php
@@ -4,11 +4,18 @@ include 'db.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-$searchWord = $_GET['search'];
+if (isset($_GET['search'])) {
+    $searchWord = $_GET['search'];
+    
+    $sql = "SELECT * FROM anzeige WHERE UPPER(titel) LIKE UPPER(:search) OR UPPER(beschreibung) LIKE UPPER(:search)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['search' => '%' . $searchWord . '%']);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$sql = "SELECT * FROM  anzeige WHERE UPPER(titel) LIKE UPPER('%$searchWord%') OR UPPER(beschreibung) LIKE UPPER('%$searchWord%')";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($result);
+} else {
+    echo json_encode([
+        "error" => "Parameter 'search' fehlt."
+    ]);
+}
 
-echo json_encode($result);
